2174a1e62d270dc35c79ea98307d4b75f0f7fb58
Bwalya Musonda <144368838+bm9985@users.noreply.github.com>
2024-03-06 13:34:35 -0500

    core function implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,148 @@
+import { useEffect, useState } from 'react';
 import styles from './App.module.css';
 
-function App() {
-	const handleEnter = () => {};
-	const handleNumberClick = (num: number | string) => {};
+const App = () => {
+	const [stack, setStack] = useState<number[]>([]);
+	const [activeNumber, setActiveNumber] = useState<string>('0');
 
-	const handleOperatorClick = (operator: string) => {};
+	const handleNumberClick = (number: number | string) => {
+		setActiveNumber((prevActiveNumber) => {
+			if (prevActiveNumber === '0') {
+				return number.toString();
+			} else {
+				return prevActiveNumber + number.toString();
+			}
+		});
+	};
+
+	const handleOperatorClick = (operator: string) => {
+		if (!isNaN(parseFloat(activeNumber))) {
+			const num1 = stack[0];
+			const num2 = parseFloat(activeNumber);
+			let result = 0;
+			const stackCopy = [...stack];
+
+			switch (operator) {
+				case '+':
+					result = num1 + num2;
+					removeFromStack();
+					break;
+				case '-':
+					if (num2 > 0) {
+						result = 0 - num2;
+					} else {
+						result = num1 - num2;
+						removeFromStack();
+					}
+					break;
+				case '*':
+				case 'x':
+				case 'X':
+					result = num1 * num2;
+					removeFromStack();
+					break;
+				case '/':
+				case '%':
+					result = num1 / num2;
+					removeFromStack();
+					break;
+				case '^':
+					result = num1 ^ num2;
+					removeFromStack();
+					break;
+				case '>':
+					if (num2 > 0) {
+						stackCopy.push(num2);
+						pushToStack(num2);
+					}
+					result = Math.max(...stackCopy);
+					break;
+				case '<':
+					if (num2 > 0) {
+						stackCopy.push(num2);
+						pushToStack(num2);
+					}
+					result = Math.min(...stackCopy);
+					break;
+				case 'log':
+					if (num2 > 0) {
+						result = Math.log10(num2);
+					}
+					break;
+				case 'pop':
+					if (stack.length > 0) {
+						result = stack.pop()!;
+					}
+					break;
+				default:
+					break;
+			}
+			setActiveNumber(result.toString());
+		}
+	};
+
+	const pushToStack = (value: number) => {
+		setStack((prevStack) => [value, ...prevStack]);
+	};
+
+	const removeFromStack = () => {
+		setStack((prevStack) => prevStack.slice(1));
+	};
+
+	const handleClear = () => {
+		setStack([]);
+		setActiveNumber('0');
+	};
+
+	const handleEnter = () => {
+		if (!isNaN(parseFloat(activeNumber))) {
+			pushToStack(parseFloat(activeNumber));
+			setActiveNumber('0');
+		}
+	};
+
+	useEffect(() => {
+		const handleKeyPress = (event: KeyboardEvent) => {
+			const key = event.key;
+			if (!isNaN(parseInt(key))) {
+				handleNumberClick(parseInt(key));
+			} else if (
+				key === '.' ||
+				key === '+' ||
+				key === '-' ||
+				key === '*' ||
+				key === '/' ||
+				key === '%' ||
+				key === '^' ||
+				key === '>' ||
+				key === '<' ||
+				key === '*' ||
+				key === 'X' ||
+				key === 'x'
+			) {
+				handleOperatorClick(key);
+			} else if (key === 'Enter') {
+				handleEnter();
+			}
+		};
+
+		window.addEventListener('keypress', handleKeyPress);
+
+		return () => {
+			window.removeEventListener('keypress', handleKeyPress);
+		};
+	}, [handleNumberClick, handleOperatorClick]);
+
 	return (
 		<>
 			<div className={styles.calculator}>
 				<div className={styles.display}>
-					<div className={styles.active}>0</div>
+					<div className={styles.stack}>
+						{stack.map((item, index) => (
+							<div key={index}>{item}</div>
+						))}
+					</div>
+					<div className={styles.active}>{activeNumber}</div>
 				</div>
 				<div className={styles.buttons}>
 					<div className={styles.row}>
@@ -43,13 +176,20 @@ function App() {
 						<button onClick={() => handleOperatorClick('pop')}>Pop</button>
 					</div>
 					<div className={styles.row}>
+						<button onClick={handleClear}>C</button>
 						<button onClick={handleEnter}>Enter</button>
 					</div>
 				</div>
 			</div>
 			<div></div>
+			<input
+				type='text'
+				id='hiddenInput'
+				className={styles.hiddenInput}
+				ref={(input) => input?.focus()}
+			/>
 		</>
 	);
-}
+};
 
 export default App;
