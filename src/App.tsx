2796652a56447f70698b16068c4c5a2e715f0230
Bwalya Musonda <144368838+bm9985@users.noreply.github.com>
2024-03-06 11:28:42 -0500

    UI implementation

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import styles from './App.module.css';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	const handleEnter = () => {};
+	const handleNumberClick = (num: number | string) => {};
+
+	const handleOperatorClick = (operator: string) => {};
+	return (
+		<>
+			<div className={styles.calculator}>
+				<div className={styles.display}>
+					<div className={styles.active}>0</div>
+				</div>
+				<div className={styles.buttons}>
+					<div className={styles.row}>
+						<button onClick={() => handleNumberClick(7)}>7</button>
+						<button onClick={() => handleNumberClick(8)}>8</button>
+						<button onClick={() => handleNumberClick(9)}>9</button>
+						<button onClick={() => handleOperatorClick('/')}>/</button>
+					</div>
+					<div className={styles.row}>
+						<button onClick={() => handleNumberClick(4)}>4</button>
+						<button onClick={() => handleNumberClick(5)}>5</button>
+						<button onClick={() => handleNumberClick(6)}>6</button>
+						<button onClick={() => handleOperatorClick('*')}>*</button>
+					</div>
+					<div className={styles.row}>
+						<button onClick={() => handleNumberClick(1)}>1</button>
+						<button onClick={() => handleNumberClick(2)}>2</button>
+						<button onClick={() => handleNumberClick(3)}>3</button>
+						<button onClick={() => handleOperatorClick('-')}>-</button>
+					</div>
+					<div className={styles.row}>
+						<button onClick={() => handleNumberClick(0)}>0</button>
+						<button onClick={() => handleNumberClick('.')}>.</button>
+						<button onClick={() => handleOperatorClick('+')}>+</button>
+						<button onClick={() => handleOperatorClick('^')}>^</button>
+					</div>
+					<div className={styles.row}>
+						<button onClick={() => handleOperatorClick('>')}>&gt;</button>
+						<button onClick={() => handleOperatorClick('<')}>&lt;</button>
+						<button onClick={() => handleOperatorClick('log')}>Log</button>
+						<button onClick={() => handleOperatorClick('pop')}>Pop</button>
+					</div>
+					<div className={styles.row}>
+						<button onClick={handleEnter}>Enter</button>
+					</div>
+				</div>
+			</div>
+			<div></div>
+		</>
+	);
 }
 
 export default App;
